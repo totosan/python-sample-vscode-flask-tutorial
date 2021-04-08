f8c5f19c9c5f897df2b6d56e09c5cc737b4d0b84
Thomas Tomow <toto_san@live.com>
2021-04-08 11:44:03 +0200

    Create test_python.yml (#4)
    * Create test_python.yml
    
    for testing purposes
    
    * changed python version to 3.8.6
    
    * changed startup.py for flake8

--- startup.py
+++ startup.py
@@ -1,18 +1,13 @@
-""" 
-In this sample, the Flask app object is contained within the hello_app *module*;
+"""
+In this sample, the Flask app object is contained within the hello_app *module*
 that is, hello_app contains an __init__.py along with relative imports. Because
 of this structure, a file like webapp.py cannot be run directly as the startup
-file through Gunicorn; the result is "Attempted relative import in non-package".
+file through Gunicorn; the result is "Attempted relative import in non-package"
 
 The solution is to provide a simple alternate startup file, like this present
 startup.py, that just imports the app object. You can then just specify
 startup:app in the Gunicorn command.
 """
-import traceback
 
 from hello_app.webapp import app
-try:
-  ex = Exception, "Alert"
-  raise ex
-except Exception as e:
-  return traceback.format_exc(e)
+print(app.name)
